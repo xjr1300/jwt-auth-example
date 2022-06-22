ed7b91262c4ce935eb5994eec544d2e7f22ee17e
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-06-22 17:12:13 +0900

    ログを出力するように設定

--- web-server/src/bin/main.rs
+++ web-server/src/bin/main.rs
@@ -1,15 +1,34 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use dotenvy::dotenv;
+use tracing_subscriber::{fmt::writer::MakeWriterExt, EnvFilter};
 
+use web_server::telemetries::{get_subscriber, init_subscriber};
+
+#[tracing::instrument(name = "Hello world")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+
+    let path = std::env::current_dir().expect("カレントディレクトリの検知に失敗しました。");
+    let log_dir = path.join("logs");
+    let log_file = tracing_appender::rolling::daily(log_dir, "web");
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let subscriber = get_subscriber(
+        "jwt-auth-example".into(),
+        env_filter,
+        std::io::stdout.and(log_file), // 標準出力とファイルにログを出力
+    );
+    init_subscriber(subscriber);
+
+    tracing::info!("startup server...");
     HttpServer::new(move || App::new().route("/", web::get().to(hello)))
         .bind("127.0.0.1:8000")?
         .run()
         .await?;
 
-    return Ok(());
+    Ok(())
 }
