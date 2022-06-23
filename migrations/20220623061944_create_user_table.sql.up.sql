df6c28179c38781dd5f98ef8b4b399d62439d9a4
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-06-23 19:24:39 +0900

    ユーザーテーブルを作成するSQL文を修正

--- migrations/20220623061944_create_user_table.sql.up.sql
+++ migrations/20220623061944_create_user_table.sql.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users(
     id UUID PRIMARY KEY,
     user_name VARCHAR(40) NOT NULL,
-    emaiL_address VARCHAR(120) NOT NULL UNIQUE,
+    email_address VARCHAR(120) NOT NULL UNIQUE,
     hashed_password TEXT NOT NULL,
     last_logged_in TIMESTAMPTZ,
     created_at TIMESTAMPTZ NOT NULL,
