95a1c5d292a4ea087f1cde5f9e7de7b07b7f25ea
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-06-30 17:37:38 +0900

    リフレッシュトークン構造体の実装場所を修正

--- domains/src/models/mod.rs
+++ domains/src/models/mod.rs
@@ -1,15 +1,3 @@
-use time::OffsetDateTime;
-
 pub mod base;
 pub mod refresh_tokens;
 pub mod users;
-
-/// リフレッシュトークン構造体
-pub struct RefreshToken {
-    /// セッションID
-    pub session_id: String,
-    /// リフレッシュトークン
-    pub refresh_token: String,
-    /// 有効期限
-    pub expired_at: OffsetDateTime,
-}
