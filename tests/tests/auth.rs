f67a3526c9f701f097da9d4bddff13e417538675
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-07-01 14:49:33 +0900

    Eメールアドレスとパスワードが正しくて、アクティブなユーザーが認証されることを確認するテストを実装

--- tests/tests/auth.rs
+++ tests/tests/auth.rs
@@ -12,7 +12,7 @@ async fn anonymous_user_unauthorized() {
         password: "anonymous-password".to_owned(),
     };
     let response = app.call_login_api(&data).await;
-
+    // 401 Unauthorizedが返却されるか確認
     assert_eq!(response.status(), reqwest::StatusCode::UNAUTHORIZED);
 }
 
@@ -31,7 +31,7 @@ async fn user_unauthorized_when_wrong_password() {
         password: "wrong-password".to_owned(),
     };
     let response = app.call_login_api(&data).await;
-
+    // 401 Unauthorizedが返却されるか確認
     assert_eq!(response.status(), reqwest::StatusCode::UNAUTHORIZED);
 }
 
@@ -50,8 +50,27 @@ async fn non_active_user_unauthorized() {
         password: app.test_users.non_active_user_password.clone(),
     };
     let response = app.call_login_api(&data).await;
-
+    // 401 Unauthorizedが返却されるか確認
     assert_eq!(response.status(), reqwest::StatusCode::UNAUTHORIZED);
 }
 
 // TODO; Eメールアドレスとパスワードが正しくて、アクティブなユーザーが認証されることを確認するテスト
+#[tokio::test]
+#[ignore]
+async fn active_user_authorized() {
+    let app = spawn_web_app().await;
+    let user = &app.test_users.active_user;
+    let data = LoginData {
+        email_address: user.email_address().value().to_owned(),
+        password: app.test_users.active_user_password.clone(),
+    };
+    let response = app.call_login_api(&data).await;
+    // 200 OKが返却されるか確認
+    assert_eq!(response.status(), reqwest::StatusCode::OK);
+
+    // TODO: 最終更新日時が更新されているか確認
+
+    // TODO: クッキーにセッションデータが記録されているか確認
+
+    // TODO: Redisにセッションデータが記録されているか確認
+}
