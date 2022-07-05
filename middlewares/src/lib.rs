a4f4edc9c95e44b8013e0d8c34e643fdba866c4f
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-07-05 21:32:04 +0900

    何もしないJwtAuthMiddlewareを実装

--- middlewares/src/lib.rs
+++ middlewares/src/lib.rs
@@ -1,8 +1,33 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::future::{ready, Ready};
+
+use actix_web::dev::{forward_ready, Service, Transform};
+
+pub struct JwtAuth;
+
+impl<S: Service<Req>, Req> Transform<S, Req> for JwtAuth {
+    type Response = S::Response;
+    type Error = S::Error;
+    type Transform = JwtAuthMiddleware<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(JwtAuthMiddleware { service }))
+    }
+}
+
+pub struct JwtAuthMiddleware<S> {
+    service: S,
+}
+
+impl<S: Service<Req>, Req> Service<Req> for JwtAuthMiddleware<S> {
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = S::Future;
+
+    forward_ready!(service);
+
+    fn call(&self, req: Req) -> Self::Future {
+        self.service.call(req)
     }
 }
