a10c99b29fb24ee805adb17370c05d0a217affed
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-07-08 12:08:23 +0900

    統合テストの実行方法を修正

--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cargo test -- --ignored \
+cargo test --package tests -- --ignored \
     --skip can_access_protected_resource_at_within_expiration_of_refresh_token \
     --skip cannot_access_protected_resource_at_expired_expiration_of_refresh_token
 
-cargo test -- --ignored \
+cargo test --package tests -- --ignored \
     can_access_protected_resource_at_within_expiration_of_refresh_token
 
-cargo test -- --ignored \
+cargo test --package tests -- --ignored \
     cannot_access_protected_resource_at_expired_expiration_of_refresh_token
